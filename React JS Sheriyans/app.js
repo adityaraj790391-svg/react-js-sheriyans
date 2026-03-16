390d4b7031501b5e58d5090974bf91d34e4decd8
Aditya Raj <adityaraj790391@gmail.com>
2026-03-17 02:28:12 +0530

    Add files via upload

---
+++ React JS Sheriyans/app.js
@@ -0,0 +1,22 @@
+// Default export - only one export in one module - name can be anything
+
+// const arr = [10, 20, 30];
+// export default arr
+
+// const user = 'Sarthak' ;
+// export default user
+
+
+
+// Named export - multiple exports - name must be the same 
+
+export const array = [10, 20, 30, 40, 50, 60];
+export const userName  = ' Shaurya ' ;
+
+
+const user = 'Sarthak' ;
+export const age = '34' ;
+export const skills = ['Frontend', 'Backend' , 'UI/UX']
+
+export default user
+
---
+++ React JS Sheriyans/script.js
@@ -0,0 +1,53 @@
+// let h1 = document.createElement('h1');
+
+// h1.innerHTML = "Hello from JS"
+
+// document.body.appendChild(h1)
+
+
+// Library - different libraries for different features - everything can be customized 
+// GSAP - for animations
+// Lenis - smooth scrolling
+// React JS - UI in websites
+
+// Framework - everything is inbuilt under one umberella - basic customization is possible - everything is in built
+// Next JS 
+// Angular
+
+
+// Export is of two types - default and named export 
+
+
+// Default export - only one export in one module - name can be anything
+
+// import array from './app.js';
+// console.log(array);
+
+// import username from './app.js';
+// console.log(username);
+
+
+
+// Named export - multiple exports - name must be the same 
+
+// import {array} from './app.js'
+// console.log(array);
+
+// import { userName } from './app.js';
+// console.log(userName);
+
+
+
+import user2 from './test.js';
+import naam from './app.js'
+
+console.log(naam, user2);
+
+import {age, skills} from './app.js'
+console.log(age, skills);
+
+
+
+
+
+
